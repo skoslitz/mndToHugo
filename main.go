ff4663a8b22432da1e49ec35ec4db720e77efa8b
Sebastian Koslitz <sebastian.koslitz@gmail.com>
2017-03-10 23:08:39 +0100

    switch to xml paring

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/xml"
 	"fmt"
-	"os"
+	// "os"
+	"io/ioutil"
 )
 
 func main() {
@@ -12,43 +13,28 @@ func main() {
 
 }
 
-/*type Item []struct {
-	Language string `json: "language"`
-}*/
-
-/*
-type Items struct {
-	Item       []interface{} `json: "item"`
-	TotalCount string        `json: total_count"`
-}*/
-
 type BlogPost struct {
-	Items struct {
-		Item []struct {
-			Published string `json: "published_at"`
-			Language  string `json: "language"`
-		} `json: "item"`
-		TotalCount string `json: total_count"`
-	} `json: "items"`
+	TotalCount string   `xml:"total-count,attr"`
+	SourceId   []string `xml:"item>source_id"`
 }
 
 func query() (BlogPost, error) {
 
-	file, err := os.Open("posts/blog_posts.json")
+	// file, err := os.Open("posts/blogposts.xml")
+	file, err := ioutil.ReadFile("posts/blogposts.xml")
+
 	if err != nil {
 		return BlogPost{}, err
 	}
 
-	defer file.Close()
-
-	var d BlogPost
-	if err := json.NewDecoder(file).Decode(&d); err != nil {
+	var post BlogPost
+	if err := xml.Unmarshal([]byte(file), &post); err != nil {
 		fmt.Println(BlogPost{}, err)
 		return BlogPost{}, err
 
 	}
 
-	fmt.Println(d.Items.TotalCount)
-	return d, nil
+	fmt.Println(len(post.SourceId))
+	return post, nil
 
 }
